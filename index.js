17a3b4b7c55e7ee73b0125f94ff7af4e312766fa
raimonvelasco <raimonvelasco6@gmail.com>
2023-04-30 04:59:30 +0200

    Add global variables

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
 const {Client, Events, GatewayIntentBits} = require('discord.js');
-const {token} = require('./config.json');
+const {token, sellchannelId, responseChannelId, inventoryChannelId, buyChannelId} = require('./config.json');
 const events = require("events");
 
 // Create a new client instance
@@ -9,10 +9,6 @@ const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits
 
 let prefix = '+';
 let prefixCommand = '!';
-const channelId = '1101980297693757493';
-const responseChannelId = '1101987891841867936';
-const inventoryChannelId = '1102053614115094618';
-const buyChannelId = '1101980297693757492';
 
 const responseChannel = client.channels.cache.get(responseChannelId);
 
@@ -26,7 +22,7 @@ client.once(Events.ClientReady, c => {
 
 
 client.on(Events.MessageCreate, async (message) => {
-        if (message.channel.id === channelId) {
+        if (message.channel.id === sellchannelId) {
             console.log(message.content);
             const [_, amount, product] = message.content.split(' ');
             console.log(_ + ' + ' + amount + ' + ' + product);
